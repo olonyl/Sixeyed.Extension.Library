97ec1f449e570c6feae8b418ce82531105e74ea1
olonyl <olony.rocha@contollo.net>
2022-01-27 22:21:13 -0600

    Add project files.

---
+++ Sixeyed.Extension.Library.Tests/Demo1/EnumExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using componentModel = System.ComponentModel;
+
+namespace Sixeyed.Extension.Library.Tests.Demo1
+{
+    public class EnumExtensionsTests
+    {
+        [Test]
+        public void GetName()
+        {
+            var intro = Module.Intro;
+            Assert.AreEqual("Intro", intro.ToString());
+            Assert.AreEqual("Intro", Enum.GetName(typeof(Module), intro));
+            Assert.AreEqual("Intro", intro.GetName());
+        }
+
+        [Test]
+        public void GetDescription()
+        {
+            Assert.AreEqual("Introducing Extension Methods", Module.Intro.GetDescription());
+            Assert.AreEqual("Advanced", Module.Advanced.GetDescription());
+
+            Assert.AreEqual("In Progress", ModuleStatus.InProgress.GetDescription());
+            Assert.AreEqual("Done", ModuleStatus.Done.GetDescription());
+        }
+
+        public enum Module
+        {
+            [componentModel.Description("Introducing Extension Methods")]
+            Intro,
+            Advanced,
+            Library
+        }
+
+        public enum ModuleStatus
+        {
+            Todo,
+            [componentModel.Description("In Progress")]
+            InProgress,
+            Done
+        }
+    }
+}
---
+++ Sixeyed.Extension.Library.Tests/Demo1/ExceptionExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Sixeyed.Extension.Library.Tests.Demo1
+{
+
+    public class ExceptionExtensionsTests
+    {
+        [Test]
+        public void DivideBy1()
+        {
+            Assert.AreEqual(10, Divide(10, 1));
+        }
+
+        [Test]
+        public void DivideBy0()
+        {
+            try
+            {
+                Divide(10, 0);
+                Assert.Fail("Should throw exception");
+            }
+            catch (Exception ex)
+            {
+                TestContext.WriteLine(ex.FullMessage());
+                Assert.IsInstanceOf(typeof(ApplicationException), ex);
+            }
+        }
+
+        private double Divide(int amount, int by)
+        {
+            try
+            {
+                return amount / by;
+            }
+            catch (Exception ex)
+            {
+                var invalidOpEx = new InvalidOperationException("Invalid operation", ex);
+                var message = string.Format("Divide failed - amount: {0}, by: {1}", amount, by);
+                throw new ApplicationException(message, invalidOpEx);
+            }
+        }
+    }
+}
