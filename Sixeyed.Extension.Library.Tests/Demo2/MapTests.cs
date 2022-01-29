d9157d3c6ba29d69c2e8d2eb890fc15a78af8fab
olonyl <olony.rocha@contollo.net>
2022-01-28 20:39:31 -0600

    Added extension method to create a Property mapper between Types

---
+++ Sixeyed.Extension.Library.Tests/Demo2/MapTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Sixeyed.Extension.Library.Tests.Demo2
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class MapTests
+    {
+        [Test]
+        public void WithStringReflection()
+        {
+            var source = new User
+            {
+                FirstName = "Elton",
+                LastName = "Stoneman",
+                BirthDate = new DateTime(1980, 1, 1)
+            };
+
+            var map = new Map<User, UserModel>(source);
+            map.Populate("DateOfBirth", "BirthDate")
+                .Populate("Name", s => s.FirstName + " " + s.LastName)
+                .Populate("Age", s => (int)DateTime.Today.Subtract(s.BirthDate).TotalDays / 365);
+
+            var target = map.Target;
+            Assert.AreEqual("Elton Stoneman", target.Name);
+            Assert.AreEqual(source.BirthDate, target.DateOfBirth);
+            Assert.AreEqual(42, target.Age);
+        }
+
+        [Test]
+        public void WithExpressionReflection()
+        {
+            var source = new User
+            {
+                FirstName = "Elton",
+                LastName = "Stoneman",
+                BirthDate = new DateTime(1980, 1, 1)
+            };
+
+            var map = new Map<User, UserModel>(source);
+            map.Populate(t => t.DateOfBirth, s => s.BirthDate)
+                .Populate(t => t.Name, s => s.FirstName + " " + s.LastName)
+                .Populate(t => t.Age, s => (int)DateTime.Today.Subtract(s.BirthDate).TotalDays / 365);
+
+            var target = map.Target;
+            Assert.AreEqual("Elton Stoneman", target.Name);
+            Assert.AreEqual(source.BirthDate, target.DateOfBirth);
+            Assert.AreEqual(42, target.Age);
+        }
+    }
+}
